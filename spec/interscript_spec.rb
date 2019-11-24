74b6c2a72a17d15ea44f9e8bc63e6ea9e5947721
Ronald Tse <ronald.tse@ribose.com>
2019-11-24 08:41:26 +0800

    Remove debugging puts from spec

--- spec/interscript_spec.rb
+++ spec/interscript_spec.rb
@@ -3,7 +3,6 @@
 require "diffy"
 
 RSpec.describe Interscript do
-
   Dir["maps/*.yaml"].each do |system_file|
     context "#{system_file} system" do
       system = YAML.load_file(system_file)
@@ -13,7 +12,7 @@ RSpec.describe Interscript do
 
         it "test for #{test}" do
           result = Interscript.transliterate system_name, test["source"]
-          puts "#{test["expected"]} result #{result}"
+          # puts "#{test["expected"]} result #{result}"
           expect(result).to eq(test["expected"])
 
           # diff = Diffy::Diff.new test["expected"], result
