095cb5769cd6885f452bbf37eac30924563cf6d8
Ronald Tse <ronald.tse@ribose.com>
2019-11-24 08:39:13 +0800

    Fix spec running method to actually show errors per test per system

--- spec/interscript_spec.rb
+++ spec/interscript_spec.rb
@@ -3,43 +3,27 @@
 require "diffy"
 
 RSpec.describe Interscript do
-  it "test all systems" do
-    match_system = ENV["TRANSLIT_SYSTEM"] || "*"
-    results = Dir["maps/#{match_system}.yaml"].reduce({}) do |sysresults, system_file|
-      puts "Testing system " + system_file + " ..."
-      system = YAML.load_file system_file
+
+  Dir["maps/*.yaml"].each do |system_file|
+    context "#{system_file} system" do
+      system = YAML.load_file(system_file)
       system_name = File.basename(system_file, ".yaml")
 
       tests = system["tests"]&.reduce([]) do |testresults, test|
-        result = Interscript.transliterate system_name, test["source"]
-        diff = Diffy::Diff.new test["expected"], result
-        testresults << { expected: test["expected"], got: result, diff: diff } unless diff.to_s.empty?
-        testresults
-      end
 
-      sysresults[system_name] = tests unless tests.nil? || tests.empty?
-      sysresults
-    end
+        it "test for #{test}" do
+          result = Interscript.transliterate system_name, test["source"]
+          puts "#{test["expected"]} result #{result}"
+          expect(result).to eq(test["expected"])
 
-    unless results.empty?
-      puts
-      warn "Failures:"
-      results.each do |name, tests|
-        puts
-        warn "  Fail system: #{name}"
-        tests.each do |test|
-          puts
-          if test[:expected].lines.count > 2
-            warn "Diff:"
-            warn test[:diff]
-          else
-            warn "    expected: #{test[:expected]}"
-            warn "         got: #{test[:got]}"
-          end
+          # diff = Diffy::Diff.new test["expected"], result
+          # puts diff.inspect
+          # expect diff.to_s.empty?
         end
-      end
-    end
 
-    expect(results).to be_empty
+      end
+
+    end
   end
+
 end
