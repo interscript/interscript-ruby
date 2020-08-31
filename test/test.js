325cae20263ffa2b623e0f647ae36afb3ec72daf
webdev778 <webdev778@gmail.com>
2020-08-31 13:56:02 +0800

    fix hound suggested issue

--- test/test.js
+++ test/test.js
@@ -1,15 +1,15 @@
 var assert = require('assert');
-const { Opal: { Interscript } } = require('../vendor/assets/javascripts/interscript.js');
+var Opal = require('../vendor/assets/javascripts/interscript.js');
 
-Object.keys(ISMap).forEach(key => {
-	const map = JSON.parse(ISMap[key]);
-	describe(`${key} system`, function () {
+Object.keys(ISMap).forEach(function(key) {
+	var map = JSON.parse(ISMap[key]);
+	describe(key+' system', function () {
 		this.timeout(10000);
-		map?.tests?.forEach(test => {
-			if(test["source"] === null || test["expected"] === null) return;
-			it(`test for ${JSON.stringify(test)}`, function () {
-				const result = Interscript.$transliterate(key, test["source"]);
-				const expected = test["expected"]?.normalize();
+		map.tests && map.tests.forEach(function(test) {
+			if(test['source'] === null || test['expected'] === null) return;
+			it('test for ' + JSON.stringify(test), function () {
+				var result = Opal.Opal.Interscript.$transliterate(key, test['source']);
+				var expected = test['expected']?.normalize();
 				// assert.strictEqual(result, expected);
 				assert.equal(!!result || result === "", true);
 			});
