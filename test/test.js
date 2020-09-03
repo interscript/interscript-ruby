e6c6496ddc174e7de491d0b1938b1254225f46a1
Ronald Tse <ronald.tse@ribose.com>
2020-09-03 08:39:44 +0800

    Fix js tests

--- test/test.js
+++ test/test.js
@@ -1,12 +1,15 @@
 var assert = require('assert');
 var Opal = require('../vendor/assets/javascripts/interscript.js');
 
-Object.keys(ISMap).forEach(function(key) {
-	var map = JSON.parse(ISMap[key]);
+Object.keys(InterscriptMaps).forEach(function(key) {
+	var map = JSON.parse(InterscriptMaps[key]);
 	describe(key+' system', function () {
 		this.timeout(10000);
 		map.tests && map.tests.forEach(function(test) {
-			if(test['source'] === null || test['expected'] === null) return;
+			
+      if (test['source'] === null || test['expected'] === null) 
+        return;
+      
 			it('test for ' + JSON.stringify(test), function () {
 				var result = Opal.Opal.Interscript.$transliterate(key, test['source']);
 				var expected = test['expected'] && test['expected'].normalize();
