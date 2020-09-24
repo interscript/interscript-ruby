10ae0b46853fb650502d5baf7eb049c797b17ad9
webdev778 <webdev778@gmail.com>
2020-09-24 22:54:34 +0800

    Add validation on authority codes, script codes and language codes #368

--- spec/interscript/filenames_spec.rb
+++ spec/interscript/filenames_spec.rb
@@ -6,10 +6,35 @@ RSpec.describe "filenames" do
     bgna bgnpcgn bis biulo bsi bt bulac by cn cnt din dmg dos easc efeo elot
     gaz ggg gki gost gsi hk icao ign iso itk jp jra kp lbmod lshk ma md mext
     mk mlc mlit mlmupc moct mofa msst mv nco nikl nrs odni rjgc royin rs sac
-    ses sfs sgk tm ua ucis un uz var xlsc yivo]
+    ses sfs sgk tm ua ucis un uz var xlsc yivo mvd stategeocadastre]
+
+  # From https://www.loc.gov/standards/iso639-2/php/code_list.php
+  iso6392 = %w[
+    aar abk ace ach ada ady afa afh afr ain aka akk alb ale alg alt amh ang anp apa ara arc arg arm arn
+    arp art arw asm ast ath aus ava ave awa aym aze bad bai bak bal bam ban baq bas bat bej bel bem ben
+    ber bho bih bik bin bis bla bnt bod bos bra bre btk bua bug bul bur byn cad cai car cau ceb cel ces
+    cha chb che chg chi chk chm chn cho chp chr chu chv chy cmc cnr cop cor cos cpe cpf cpp cre crh crp
+    csb cus cym cze dak dan dar day del den deu dgr din div doi dra dsb dua dum dut dyu dzo efi egy eka
+    ell elx eng enm epo est eus ewe ewo fan fao fas fat fij fil fin fiu fon fra fre frm fro frr frs fry
+    ful fur gaa gay gba gem geo ger gez gil gla gle glg glv gmh goh gon gor got grb grc gre grn gsw guj
+    gwi hai hat hau haw heb her hil him hin hit hmn hmo hrv hsb hun hup hye iba ibo ice ido iii ijo iku
+    ile ilo ina inc ind ine inh ipk ira iro isl ita jav jbo jpn jpr jrb kaa kab kac kal kam kan kar kas
+    kat kau kaw kaz kbd kha khi khm kho kik kin kir kmb kok kom kon kor kos kpe krc krl kro kru kua kum
+    kur kut lad lah lam lao lat lav lez lim lin lit lol loz ltz lua lub lug lui lun luo lus mac mad mag
+    mah mai mak mal man mao map mar mas may mdf mdr men mga mic min mis mkd mkh mlg mlt mnc mni mno moh
+    mon mos mri msa mul mun mus mwl mwr mya myn myv nah nai nap nau nav nbl nde ndo nds nep new nia nic
+    niu nld nno nob nog non nor nqo nso nub nwc nya nym nyn nyo nzi oci oji ori orm osa oss ota oto paa
+    pag pal pam pan pap pau peo per phi phn pli pol pon por pra pro pus qaa-qtz que raj rap rar roa roh
+    rom ron rum run rup rus sad sag sah sai sal sam san sas sat scn sco sel sem sga sgn shn sid sin sio
+    sit sla slk slo slv sma sme smi smj smn smo sms sna snd snk sog som son sot spa sqi srd srn srp srr
+    ssa ssw suk sun sus sux swa swe syc syr tah tai tam tat tel tem ter tet tgk tgl tha tib tig tir tiv
+    tkl tlh tli tmh tog ton tpi tsi tsn tso tuk tum tup tur tut tvl twi tyv udm uga uig ukr umb und urd
+    uzb vai ven vie vol vot wak wal war was wel wen wln wol xal xho yao yap yid yor ypk zap zbl zen zgh
+    zha
+  ]
 
   # From https://iso639-3.sil.org/code_tables/download_tables
-  valid_langs = %w[aaa aab aac aad aae aaf aag aah aai aak aal aan aao aap aaq aar aas aat aau aaw aax
+  iso6393 = %w[aaa aab aac aad aae aaf aag aah aai aak aal aan aao aap aaq aar aas aat aau aaw aax
     aaz aba abb abc abd abe abf abg abh abi abj abk abl abm abn abo abp abq abr abs abt abu abv abw abx
     aby abz aca acb acd ace acf ach aci ack acl acm acn acp acq acr acs act acu acv acw acx acy acz ada
     adb add ade adf adg adh adi adj adl adn ado adq adr ads adt adu adw adx ady adz aea aeb aec aed aee
@@ -325,6 +350,7 @@ RSpec.describe "filenames" do
     zpu zpv zpw zpx zpy zpz zqe zra zrg zrn zro zrp zrs zsa zsk zsl zsm zsr zsu zte ztg ztl ztm ztn ztp
     ztq zts ztt ztu ztx zty zua zuh zul zum zun zuy zwa zxx zyb zyg zyj zyn zyp zza zzj]
 
+  valid_langs = (iso6392 << iso6393).flatten.uniq
   # From https://www.unicode.org/iso15924/iso15924-codes.html
   valid_script_codes = %w[Adlm Afak Aghb Ahom Arab Aran Armi Armn Avst Bali
     Bamu Bass Batk Beng Bhks Blis Bopo Brah Brai Bugi Buhd Cakm Cans Cari Cham
