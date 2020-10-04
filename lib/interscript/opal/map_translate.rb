71d3255c6de7e405193e2d45fae17dc176a0858d
webdev778 <webdev778@gmail.com>
2020-10-04 20:29:31 +0200

    Migrate to opal-onigmo

--- lib/interscript/opal/map_translate.rb
+++ lib/interscript/opal/map_translate.rb
@@ -1,12 +1,7 @@
 module Interscript
   module OpalMapTranslate
     def self.translate_regexp(src)
-      src.
-        gsub('[:upper:]', '\\\\\\\\p{Lu}').
-        gsub('[:lower:]', '\\\\\\\\p{Ll}').
-        gsub('[:alpha:]', '\\\\\\\\p{L}').
-        gsub('(?<=[\\\\p{Lu}])?', '(?<=[\\\\\\\\p{Lu}]?)').
-        gsub('(?=[\\\\p{Lu}])?', '(?=[\\\\\\\\p{Lu}]?)')
+      src
     end
   end
 end
