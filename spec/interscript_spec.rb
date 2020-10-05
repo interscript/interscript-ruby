3062037533152d86dcd9b6b39f9644dc22b41b2f
Alexande B <abobrikovich@gmail.com>
2020-10-05 16:02:27 +0300

    Fail if TRANSLIT_SYSTEM don't match anything

--- spec/interscript_spec.rb
+++ spec/interscript_spec.rb
@@ -6,8 +6,11 @@ include PyCall::Import
 
 RSpec.describe Interscript do
   mask = ENV["TRANSLIT_SYSTEM"] || "*"
-  maps = {}
-  Dir["maps/#{mask}.yaml"].each do |system_file|
+  maps = Dir["maps/#{mask}.yaml"]
+
+  fail "TRANSLIT_SYSTEM env didn't match any map configuration" if maps.empty?
+
+  maps.each do |system_file|
     context "#{system_file} system" do
       system = YAML.load_file(system_file)
       system_name = File.basename(system_file, ".yaml")
@@ -15,8 +18,8 @@ RSpec.describe Interscript do
       system["tests"]&.reduce([]) do |_, test|
         it "test for #{test}" do
           Timeout::timeout(5) do
-            result = Interscript.transliterate(system_name, test["source"], maps) || ""
-            expected = (test["expected"] || "").unicode_normalize
+            result = Interscript.transliterate(system_name, test["source"])
+            expected = test["expected"]&.unicode_normalize
             expect(result).to eq(expected)
           end
         end
