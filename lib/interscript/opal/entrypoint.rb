7b22c8b89273c66b2fa051cb0141e474a713c849
webdev778 <webdev778@gmail.com>
2020-10-05 16:03:18 +0200

    Support asynchronous loading

--- lib/interscript/opal/entrypoint.rb
+++ lib/interscript/opal/entrypoint.rb
@@ -1,2 +1,12 @@
 require "opal"
-require "interscript"
+require "onigmo/onigmo-wasm"
+
+module Interscript
+  def self.on_load(&block)
+    WebAssembly.wait_for("onigmo/onigmo-wasm", &block)
+  end
+end
+
+Interscript.on_load do
+  require "interscript"
+end
