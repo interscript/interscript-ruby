95fc34c84ec10282cfa16ede3abb4a8646cbb036
webdev778 <webdev778@gmail.com>
2020-10-20 13:02:46 +0200

    Fixup: Add exports.rb

---
+++ lib/interscript/opal/exports.rb
@@ -0,0 +1,11 @@
+# Exports - the methods used publically. Listed so that
+# opal-optimizer will know the methods it needs to preserve.
+
+Interscript.on_load
+Interscript.on_load_maps
+Interscript.load_maps
+Interscript.load_map_json
+Interscript.transliterate
+
+# TODO: Fix handling of "ord".$to_proc() in opal-optimizer
+"a".ord
