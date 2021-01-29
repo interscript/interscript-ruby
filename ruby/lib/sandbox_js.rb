78e9f1735a65aa82de4a4d4f32cf6528ff1b713d
webdev778 <webdev778@gmail.com>
2021-01-29 11:14:13 +0100

    Remove sandboxes, those have no place in release code and are superseded by tests. Maybe in the future a part of this can be reintroduced as examples.

--- ruby/lib/sandbox_js.rb
+++
@@ -1,8 +0,0 @@
-$: << "."
-
-require 'pry'
-require 'interscript'
-require 'interscript/compiler/javascript'
-
-fname = 'iso-kor-Hang-Latn-1996-method1'
-puts Interscript::Compiler::Javascript.(fname).code
