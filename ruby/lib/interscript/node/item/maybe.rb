f60cf663897d696ca4caaca0d5ee704c2d094feb
webdev778 <webdev778@gmail.com>
2021-03-11 12:24:36 +0900

    Port odni-kor-Hang-Latn-2015; small fixes

--- ruby/lib/interscript/node/item/maybe.rb
+++ ruby/lib/interscript/node/item/maybe.rb
@@ -19,7 +19,44 @@ class Interscript::Node::Item::Maybe < Interscript::Node::Item
       :data => self.data.to_hash }
   end
 end
-class Interscript::Node::Item::MaybeSome < Interscript::Node::Item::Maybe
+class Interscript::Node::Item::MaybeSome < Interscript::Node::Item
+  attr_accessor :data
+  def initialize data
+    data = Interscript::Node::Item.try_convert(data)
+    @data = data
+  end
+
+  def first_string
+    data.first_string
+  end
+
+  def max_length
+    data.max_length
+  end
+
+  def to_hash
+    { :class => self.class.to_s,
+      :data => self.data.to_hash }
+  end
 end
-class Interscript::Node::Item::Some < Interscript::Node::Item::Maybe
+
+class Interscript::Node::Item::Some < Interscript::Node::Item
+  attr_accessor :data
+  def initialize data
+    data = Interscript::Node::Item.try_convert(data)
+    @data = data
+  end
+
+  def first_string
+    data.first_string
+  end
+
+  def max_length
+    data.max_length
+  end
+
+  def to_hash
+    { :class => self.class.to_s,
+      :data => self.data.to_hash }
+  end
 end
