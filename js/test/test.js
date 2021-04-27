542a504b28d3d1adcf1defd0dd9d26b987a983c6
webdev778 <webdev778@gmail.com>
2021-04-27 11:37:51 +0900

    Restore v1 functionality of listing all maps

--- js/test/test.js
+++ js/test/test.js
@@ -2,6 +2,14 @@ var assert = require('assert');
 var Interscript = require('../src/stdlib');
 
 describe('interscript-js', function() {
+  it('contains a list of available maps - at first 0, but later a correct value', async function () {
+    var mapcount = Object.keys(Interscript.maps).length;
+    assert.ok(mapcount == 0);
+    await Interscript.load_map_list();
+    mapcount = Object.keys(Interscript.maps).length;
+    assert.ok(mapcount > 100);
+  });
+
   it('can transliterate', async function () {
     var src = "Антон";
     await Interscript.load_map('bgnpcgn-ukr-Cyrl-Latn-2019');
