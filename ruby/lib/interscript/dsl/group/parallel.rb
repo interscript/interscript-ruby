b32a437a6b958f4fdbe44535bff40f5ed28864e2
webdev778 <webdev778@gmail.com>
2021-07-30 06:19:34 +0900

    Restore Ruby 2.5 compat

--- ruby/lib/interscript/dsl/group/parallel.rb
+++ ruby/lib/interscript/dsl/group/parallel.rb
@@ -1,6 +1,6 @@
 class Interscript::DSL::Group::Parallel < Interscript::DSL::Group
-  def initialize(&block)
-    @node = Interscript::Node::Group::Parallel.new
+  def initialize(reverse_run: nil, &block)
+    @node = Interscript::Node::Group::Parallel.new(reverse_run: reverse_run)
     self.instance_exec(&block)
   end
 end
